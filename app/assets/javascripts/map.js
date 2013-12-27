0790e0094f4f8d19044aa0042fec92583080fc2c
Steven Nunez <snunez@cyrusinnovation.com>
2013-12-26 20:04:19 -0500

    Finish simple map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,3 +1,17 @@
 /**
  * Created by StevenNunez on 12/26/13.
  */
+$(function() {
+    handler = Gmaps.build('Google');
+    handler.buildMap({ provider: {}, internal: {id: 'map'}}, function () {
+        markers = handler.addMarkers([
+            {
+                "lat": 40.75387588027257,
+                "lng": -73.98961368372976,
+                "infowindow": "My Message "
+            }
+        ]);
+        handler.bounds.extendWith(markers);
+        handler.fitMapToBounds();
+    });
+});
