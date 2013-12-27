b15e9614122862bd3fc9431eda55bc1978f291d3
Steven Nunez <snunez@cyrusinnovation.com>
2013-12-26 19:36:50 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MbMap::Application.config.secret_key_base = '2de2310fadbbe3a3b41ec8b691506c7e25c387cd795bb9ad226e2eb8951ab709e6e7211fc9ad0b66e01893097718bc2ab54afa10e1294b5c2f802aad113d2a41'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MbMap::Application.config.session_store :cookie_store, key: '_mb_map_session'
